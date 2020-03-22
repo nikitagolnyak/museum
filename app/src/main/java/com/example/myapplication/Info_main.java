801e097645205178d27389e6254f7d280fb95d10
nikitagolnak <nikitoz18@gmail.com>
2020-03-23 02:49:56 +0300

    info view

--- app/src/main/java/com/example/myapplication/Info_main.java
+++ app/src/main/java/com/example/myapplication/Info_main.java
@@ -16,17 +16,8 @@ public class Info_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
